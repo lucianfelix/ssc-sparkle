d20a855cfb1cf0b13e9bda8c6691e0ae40ac0b85
Ghita Lucian <ghita@adobe.com>
2023-05-19 10:04:15 +0200

    above fold fix

--- components/AdventureCard.jsx
+++ components/AdventureCard.jsx
@@ -17,7 +17,7 @@ import Link from 'next/link'
 import Image from 'next/image';
 
 export default function AdventureCard({ _path, href, title, price, duration, imageSrc }) {
-  const aboveFold = ['Bali Surf Camp'].includes(title);
+  const aboveFold = imageSrc.includes("bali-surf-camp");
   return (
     <div key={_path} className="group relative">
       <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
